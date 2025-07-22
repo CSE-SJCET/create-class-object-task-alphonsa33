a5939666665f3dfcc03e56faf13ad5fe447cc786
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2025-07-22 21:54:04 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
-class Student{
-        String name;
-        int rollNumber;
-        char grade;
+class Student {
+    String name;
+    int rollNumber;
+    char grade;
 
-public void displayDetails() {
-        System.out.println("Name:"+name);
-        System.out.println("rollNumber:"+rollNumber);
-        System.out.println("grade:"+grade);
-}
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+    }
 }
 
 public class Main {
-      public static void main(String[] args) {
+    public static void main(String[] args) {
         Student student = new Student();
         student.name = "alph";
         student.rollNumber = 13;
         student.grade = 'A';
         student.displayDetails();
-    }
-}     
+    }
+}
+
